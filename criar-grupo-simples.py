0e6a2eb6ba01ae869352600fdb7fe3240ca9c2da
Luis Otávio S. e Souza <luis_tavio@hotmail.com>
2023-12-08 15:33:59 -0300

    Update: criar-grupo-simples.py
    - VERIFICAÇÃO SE O GRUPO JÁ EXISTE
    - ATUALIZAÇÃO DA FOTO DO PERFIL DO GRUPO

--- criar-grupo-simples.py
+++ criar-grupo-simples.py
@@ -1,27 +1,27 @@
 from telethon import TelegramClient
-from telethon.tl.functions.messages import CreateChatRequest
-from telethon.tl.types import InputPeerUser
+from telethon.tl.functions.messages import CreateChatRequest, EditChatPhotoRequest
+from telethon.tl.types import InputChatUploadedPhoto
 
 api_id = 'seu_api_id'
 api_hash = 'seu_api_hash'
+group_name = 'Nome do Grupo'
+group_photo = 'logo.jpg'
 
 client = TelegramClient('nome_do_seu_app', api_id, api_hash)
 
 async def main():
-    # Getting information about yourself
-    me = await client.get_me()
-    print(me.stringify())
-
-    # Criando um novo grupo
-    users = ['@nome_de_usuario']  # substitua com os nomes de usuário reais
-    response = await client(CreateChatRequest(users, 'Nome do Grupo'))
-
-    # Adicionando um usuário a um grupo
-    user_to_add = client.get_input_entity('nome_de_usuario')  # substitua com o nome de usuário real
-    group_to_add = client.get_input_entity('Nome do Grupo')  # substitua com o nome do grupo real
-    client(InviteToChannelRequest(group_to_add, [user_to_add]))
-
-    # print(response.stringify())
+    dialogs = await client.get_dialogs()
+    for dialog in dialogs:
+        if dialog.name == group_name:
+            print('O grupo já existe.')
+            break
+    else:
+        print('Criando o grupo...')
+        users = []
+        response = await client(CreateChatRequest(users, group_name))
+        
+        file = await client.upload_file(group_photo) # CARREGANDO A FOTO PARA O GRUPO
+        await client(EditChatPhotoRequest(response.chats[0].id, InputChatUploadedPhoto(file))) # ATUALIZANDO A FOTO DO PERFIL DO GRUPO
 
 with client:
     client.loop.run_until_complete(main())
